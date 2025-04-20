35c9fd9bd6830c391b21a51b8451f573ebc662ce
fonfoni <143889320+fonfoni@users.noreply.github.com>
2025-04-21 04:08:35 +0700

    Update version

--- run.ps1
+++ run.ps1
@@ -378,7 +378,7 @@ if (!($version -and $version -match $match_v)) {
     }
     else {  
         # latest tested version for Win 10-12 
-        $onlineFull = "1.2.61.431.g05f88175-2116" 
+        $onlineFull = "1.2.62.575.ge8b4160e-530" 
     }
 }
 else {
